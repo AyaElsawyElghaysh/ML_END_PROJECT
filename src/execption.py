33a72e1fbc486dfde6c48aa8cc44c81d67fb5437
AyaElsawyElghaysh <ayah.elghaysh.12345@gmail.com>
2024-09-20 01:15:52 -0700

    logger&exeptionadded

--- src/execption.py
+++ src/execption.py
@@ -1,18 +1,34 @@
 import sys
+from src.loggers import logging
+
+import sys
+
+def get_error_messages(error, error_details: sys):
+    _, _, file_exc = error_details.exc_info()
+    file_name = file_exc.tb_frame.f_code.co_filename
+    error_message = f'The error occurred in file {file_name} at line number {file_exc.tb_lineno}. Error message: {str(error)}'
+    return error_message
+
+class MyCustomException(Exception):
+    def __init__(self, error_message, error_details: sys):
+        super().__init__(error_message)
+        self.error_message = get_error_messages(error_message, error_details)
+
+    def __str__(self) -> str:
+        return self.error_message
 
 
-def get_error_messages(error, error_details:sys):
-   _,_,file_exc=error_details.exc_info()
-   file_name=file_exc.tb_frame.f_code.co_filename
-   error_message=f'the error occured in file {file_name} at line  number{file_exc.tb_lineno} error message :{str(error)} '
-   return error_message
 
 
-class MyCustomExpection(Exception):
-   def __init__(self,error_message,error_details:sys):
-      super().__init__(error_message)
-      error_message=get_error_messages(error_message,error_details)
+#test this script
+# if __name__ == "__main__":
+#     try:
+#        a=1/0
+       
+#     except Exception as e:
+#        logging.info("devision by zero not allowed")
+#        raise MyCustomException(e,sys)
+       
 
-   def __str__(self) -> str:
-      return self.error_message
-
+       
+   
--- src/loggers.py
+++ src/loggers.py
@@ -1,20 +1,20 @@
+import os
 import logging
 from datetime import datetime
-import os
 
-LOG_FILE=f"{datetime.now().strftime('%m_%d_%y_%H_%M_%S')}.log "
-logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
-os.makedirs(logs_path,exist_file=True)
-LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
+# Ensure the logs directory exists
+log_directory = os.path.join(os.getcwd(), "logs")
+os.makedirs(log_directory, exist_ok=True)  # Creates the directory if it doesn't exist
 
+# Set up logging
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%y_%H_%M_%S')}.log"
+log_path = os.path.join(log_directory, LOG_FILE)
 
 logging.basicConfig(
-
-    filename=LOG_FILE_PATH,
-    format="[%(asctime)s ]%(lineno)-15s %(user)-8s %(message)s",
-    level=logging.INFO
-
+    filename=log_path,
+    level=logging.INFO,
+    format='[%(asctime)s] %(levelname)s: %(message)s'
 )
 
-
-
+# if __name__ == "__main__":
+#     logging.info("LOGGING HAS STARTED")
